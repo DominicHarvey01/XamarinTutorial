dbfc172b1e591aa78d2184aaec82f7ef1ef36047
Dominic Harvey <dominic.harvey@sita.aero>
2019-06-23 11:37:14 -0400

    Created Pizza class.  Added JSON.net package.  Reading JSON data (emulating server data in variable) and create Pizza object from it.

---
+++ Pizza/Pizza/Pizza/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaInternetConsole
+{
+    class Pizza
+    {
+        private string nom;
+        private int prix;
+        string[] ingredients;
+
+        public Pizza(string nom, int prix, string[] ingredients)
+        {
+            this.nom = nom;
+            this.prix = prix;
+            this.ingredients = ingredients;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Pizza:" + nom + " - " + prix + " $ ");
+            Console.WriteLine(String.Join(", ", ingredients));
+            Console.WriteLine("");
+
+        }
+    }
+}
--- Pizza/Pizza/Pizza/Program.cs
+++ Pizza/Pizza/Pizza/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pizza
+/**
+ * Add Json.net package Nuget
+ * See https://jsoneditoronline.org/ to create /manipulate JSON package.
+ */
+ using Newtonsoft.Json;
+
+
+
+
+namespace PizzaInternetConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string pizzasJson = "[{\"nom\":\"4 fromage\",\"prix\":\"10\",  \"ingredients\":[\"fromage1\",\"fromage2\",\"fromage3\",\"fromage4\"]},{\"nom\":\"Vegé\",\"prix\":\"7\",\"ingredients\":[\"Tomate\", \"Poivron\"]}]";
+
+            // Loading json data into variable.
+            List<Pizza> pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
+
+            foreach (Pizza p in pizzas)
+            {
+                p.Afficher();
+            }
+
         }
     }
 }
