e01a4de50efd2cf8ae30770ecdc3a833abc58c05
Dominic Harvey <dominic.harvey@sita.aero>
2019-06-23 12:56:10 -0400

    Added connection to remote json server.

--- Pizza/Pizza/Pizza/Program.cs
+++ Pizza/Pizza/Pizza/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 /**
  * Add Json.net package Nuget
  * See https://jsoneditoronline.org/ to create /manipulate JSON package.
  */
- using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 
 
@@ -19,10 +20,46 @@ namespace PizzaInternetConsole
     {
         static void Main(string[] args)
         {
-            string pizzasJson = "[{\"nom\":\"4 fromage\",\"prix\":\"10\",  \"ingredients\":[\"fromage1\",\"fromage2\",\"fromage3\",\"fromage4\"]},{\"nom\":\"Vegé\",\"prix\":\"7\",\"ingredients\":[\"Tomate\", \"Poivron\"]}]";
+            const string URL = "https://codeavecjonathan.com/res/pizzas1.json";
+
+            //string pizzasJson = "[{\"nom\":\"4 fromage\",\"prix\":\"10\",  \"ingredients\":[\"fromage1\",\"fromage2\",\"fromage3\",\"fromage4\"]},{\"nom\":\"Vegé\",\"prix\":\"7\",\"ingredients\":[\"Tomate\", \"Poivron\"]}]";
+
+            /*
+             * "using" will distroy the WebClient after the closed }
+             */
+            string pizzasJson = "";
+
+
+            using (var WebClient = new WebClient())
+            {
+                try
+                {
+                    pizzasJson = WebClient.DownloadString(URL);
+                }
+                   catch(WebException e)
+                {
+                    string message = e.Message;
+
+                    if (e.Status == WebExceptionStatus.ProtocolError)
+                    {
+                        message = "Vérifier votre url.";
+                    }
+
+                    if (e.Status == WebExceptionStatus.NameResolutionFailure)
+                    {
+                        message = "Vérifier votre accès réseaux.";
+                    }
+
+                    Console.WriteLine("ERROR: "+ message);
+                    return;
+                }
+            }
+            
 
             // Loading json data into variable.
             List<Pizza> pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
+
+
 
             foreach (Pizza p in pizzas)
             {
