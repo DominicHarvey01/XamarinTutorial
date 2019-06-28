3dc9be621898a2464eceff993561f8d31e5cc571
Dominic Harvey <dominic.harvey@sita.aero>
2019-06-27 23:33:46 -0400

    Moved stars into reutilizable content view

--- NombreMagique/NombreMagique/NombreMagique/NombreMagique/Views/GamePage.xaml.cs
+++ NombreMagique/NombreMagique/NombreMagique/NombreMagique/Views/GamePage.xaml.cs
@@ -58,9 +58,9 @@ namespace NombreMagique.Views
             if (user_input == magic_number)
             {
                 //you win
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 WinAction();// DH - no need to wait.
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 return;
             }
             else if (user_input < magic_number)
--- NombreMagique/NombreMagique/NombreMagique/NombreMagique/Views/StarsView.xaml.cs
+++ NombreMagique/NombreMagique/NombreMagique/NombreMagique/Views/StarsView.xaml.cs
@@ -10,40 +10,25 @@ using Xamarin.Forms.Xaml;
 namespace NombreMagique.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class WelcomePage : ContentPage
+    public partial class StarsView : ContentView
     {
-        public WelcomePage()
+        public StarsView()
         {
             InitializeComponent();
-
-            //hiding navigation bar
-            NavigationPage.SetHasNavigationBar(this, false);
-
             #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            //InfinateRotation(star1, 5000);
-            //InfinateRotation(star2, 7000);
-            //InfinateRotation(star3, 9000);
-            InfinateScale(playButton, 1000);
-
+            InfinateRotation(star1, 5000);
+            InfinateRotation(star2, 7000);
+            InfinateRotation(star3, 9000);
             #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
-
-        
-        private async Task InfinateScale(View view, uint length)
+        private async Task InfinateRotation(View view, uint length)
         {
             bool always = true;
             while (always)
             {
-                await view.ScaleTo(1.1, length);
-                await view.ScaleTo(1.0, length);
+                await view.RotateTo(360, length);
                 view.Rotation = 0;
             }
-        }
-
-        private void Play_Button_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new GamePage());
-
         }
     }
 }
