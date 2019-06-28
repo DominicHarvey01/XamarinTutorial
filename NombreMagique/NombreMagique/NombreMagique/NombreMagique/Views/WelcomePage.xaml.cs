4f5521e2b6729c31c0977ba86cc4a379317cb19c
Dominic Harvey <dominic.harvey@sita.aero>
2019-06-27 22:07:57 -0400

    completed main page and test page.

---
+++ NombreMagique/NombreMagique/NombreMagique/NombreMagique/Views/WelcomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NombreMagique.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+
+            //hiding navigation bar
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Play_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GamePage());
+
+        }
+    }
+}
