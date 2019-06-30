02e5a9d65ae4442736851711bb3a4942c3d52c75
Dominic Harvey <dominic.harvey@sita.aero>
2019-06-30 16:56:11 -0400

    added sandbox flexpage

--- NombreMagique/NombreMagique/NombreMagique/NombreMagique/App.xaml.cs
+++ NombreMagique/NombreMagique/NombreMagique/NombreMagique/App.xaml.cs
@@ -11,7 +11,7 @@ namespace NombreMagique
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new WelcomePage()); 
+            MainPage = new NavigationPage(new FlexPage()); 
         }
 
         protected override void OnStart()
---
+++ NombreMagique/NombreMagique/NombreMagique/NombreMagique/Views/FlexPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NombreMagique.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlexPage : ContentPage
+    {
+        public FlexPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void MyBtn_Clicked(object sender, EventArgs e)
+        {
+            var label = new Label();
+            label.Text = "test";
+            myFlexLayout.Children.Add(label);
+        }
+    }
+}
